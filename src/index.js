bac6113f0307274f5bba18b9dc6c859dfb3033fd
Berkay Akgürgen <80064960+BerkayAkgurgen@users.noreply.github.com>
2021-03-17 23:14:32 +0300

    Request sınıfı yazıldı ve test edildi.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-console.log("Test");
-console.log("Test 2");
-console.log("Test 3");
+import {Request} from "./request"
+
+const form = document.getElementById("employee-form")
+const nameInput = document.getElementById("name")
+const departmentInput = document.getElementById("department")
+const salaryInput = document.getElementById("salary")
+const employeeList = document.getElementById("employees")
+const updateEmployeeButton = document.getElementById("update")
+
+const request = new Request("http://localhost:3000/employees")
+
+// request.get()
+// .then(emp => console.log(emp))
+// .catch(err => console.log(err))
+
+// request.post({name:"Serhat Say",department:"Pazarlama",salary:3800})
+// .then(emp => console.log(emp))
+// .catch(err => console.log(err))
+
+// request.put(4,{name:"Gökhan Uçak",department:"Marketing",salary:3500})
+// .then(emp => console.log(emp))
+// .catch(err => console.log(err))
+
+request.delete(6)
+.then(emp => console.log(emp))
+.catch(err => console.log(err))
